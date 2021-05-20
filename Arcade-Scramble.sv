8711326e2128e52a236d37f957f6afb8f000e3db
misteraddons <51079966+misteraddons@users.noreply.github.com>
2021-05-20 14:00:13 -0600

    Added Positional Joystick Mapping

--- Arcade-Scramble.sv
+++ Arcade-Scramble.sv
@@ -124,6 +124,7 @@ localparam CONF_STR = {
 	"R0,Reset;",
 	"J1,Fire 1,Fire 2,Fire 3,Fire 4,Start 1P,Start 2P,Coin;",
 	"jn,A,B,X,Y,Start,Select,R;",
+	"jp,B,A,Y,X,Start,,Select;",
 	"V,v",`BUILD_DATE
 };
 
